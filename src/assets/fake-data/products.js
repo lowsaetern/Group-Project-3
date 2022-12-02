a80b004ec76c5f86a54139dabce6b795f1e6263e
Low Saetern <saetern.low123@yahoo.com>
2022-12-01 17:40:44 -0800

    update

--- src/assets/fake-data/products.js
+++ src/assets/fake-data/products.js
@@ -12,33 +12,53 @@ const products = [
         size: ["s", "m", "l", "xl"]
     },
     {
-            title: "Not for sale",
-            price: '13.99',
-            image01: product_02,
-            categorySlug: "shirt",
-            slug: "Tshirt",
-            size: ["s", "m", "l", "xl"]
-        },
-        {
-                title: "Black Shirt",
-                price: '16.99',
-                image01: product_03,
-                categorySlug: "shirt",
-                slug: "Tshirt",
-                size: ["s", "m", "l", "xl"]
-        },
+        title: "Not for sale",
+        price: '13.99',
+        image01: product_02,
+        categorySlug: "shirt",
+        slug: "Tshirt",
+        size: ["s", "m", "l", "xl"]
+    },
+    {
+        title: "Black Shirt",
+        price: '16.99',
+        image01: product_03,
+        categorySlug: "shirt",
+        slug: "Tshirt",
+        size: ["s", "m", "l", "xl"]
+    },
     ]
     const getAllProducts = () => products
 
-    const getProducts = (count) => {
-        const max = products.length - count
-        const min = 0
-        const start = Math.floor(Math.random() * (max - min) + min)
-        return products.slice(start, start + count)
+const getProducts = (count) => {
+    const max = products.length - count
+    const min = 0
+    const start = Math.floor(Math.random() * (max - min) + min)
+    return products.slice(start, start + count)
+}
+
+const getProductBySlug = (slug) => products.find(e => e.slug === slug)
+
+const getCartItemsInfo = (cartItems) => {
+    let res = []
+    if (cartItems.length > 0) {
+        cartItems.forEach(e => {
+            let product = getProductBySlug(e.slug)
+            res.push({
+                ...e,
+                product: product
+            })
+        })
     }
-    
-    const productData = {
-        getAllProducts,
-        getProducts
-    }
-    export default productData
+
+    return res.sort((a, b) => a.id > b.id ? 1 : (a.id < b.id ? -1 : 0))
+}
+
+const productData = {
+    getAllProducts,
+    getProducts,
+    getProductBySlug,
+    getCartItemsInfo
+}
+
+export default productData
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,26 +3,26 @@ import PropTypes from 'prop-types'
 
 const Button = props => {
 
-    const bg = props.backgroundColor ? 'bg-' + props.backgroundColor : 'bg-main'
+const bg = props.backgroundColor ? 'bg-' + props.backgroundColor : 'bg-main'
 
-    const size = props.size ? 'btn-' + props.size : ''
+const size = props.size ? 'btn-' + props.size : ''
 
-    const animate = props.animate ? 'btn-animate' : ''
+const animate = props.animate ? 'btn-animate' : ''
 
-    return (
-        <button
-            className={`btn ${bg} ${size} ${animate}`}
-            onClick={props.onClick ? () => props.onClick() : null}
-        >
-            <span className="btn_txt">{props.children}</span>
-            {
-                props.icon ? (
-                    <span className="btn_icon">
-                        <i className={`${props.icon} bx-tada`}></i>
-                    </span>
-                ) : null
-            }
-        </button>
+return (
+    <button
+        className={`btn ${bg} ${size} ${animate}`}
+        onClick={props.onClick ? () => props.onClick() : null}
+    >
+    <span className="btn_txt">{props.children}</span>
+    {
+        props.icon ? (
+        <span className="btn_icon">
+        <i className={`${props.icon} bx-tada`}></i>
+        </span>
+        ) : null
+    }
+    </button>
     )
 }
 
