c2ed2786f0a9dce7bd2bfcbe3fa4d10600e10511
Low Saetern <saetern.low123@yahoo.com>
2022-12-01 16:27:18 -0800

    home page and catalog

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,24 +5,26 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 
+
 import Routes from '../routes/Routes'
 
 const Layout = () => {
-  return (
-   <BrowserRouter>
-   <Route render={props => (
-    <div>
-      <div className="container">
-        <Header {...props}/>
-        <div className="main">
-          <Routes/>
-        </div>
-      </div>
-      <Footer/>
-    </div>
-   )}/> 
-   </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Route render={props => (
+                <div>
+                    <Header {...props}/>
+                    <div className="container">
+                        <div className="main">
+                            <Routes/>
+                        </div>
+                    </div>
+                    <Footer/>
+                    
+                </div>
+            )}/>
+        </BrowserRouter>
+    )
 }
 
 export default Layout
---
+++ src/components/ProductView.jsx
@@ -0,0 +1,211 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+import { withRouter } from 'react-router'
+
+import { useDispatch } from 'react-redux'
+
+import { addItem } from '../redux/shopping-cart/cartItemsSlide'
+import { remove } from '../redux/product-modal/productModalSlice'
+
+import Button from './Button'
+import numberWithCommas from '../utils/numberWithCommas'
+
+const ProductView = props => {
+
+    const dispatch = useDispatch()
+
+    let product = props.product
+
+    if (product === undefined) product = {
+        title: "",
+        price: '',
+        image01: null,
+        categorySlug: "",
+        colors: [],
+        slug: "",
+        size: [],
+        description: ""
+    }
+
+    const [previewImg, setPreviewImg] = useState(product.image01)
+
+    const [descriptionExpand, setDescriptionExpand] = useState(false)
+
+    const [color, setColor] = useState(undefined)
+
+    const [size, setSize] = useState(undefined)
+
+    const [quantity, setQuantity] = useState(1)
+
+    const updateQuantity = (type) => {
+        if (type === 'plus') {
+            setQuantity(quantity + 1)
+        } else {
+            setQuantity(quantity - 1 < 1 ? 1 : quantity - 1)
+        }
+    }
+
+    useEffect(() => {
+        setPreviewImg(product.image01)
+        setQuantity(1)
+        setColor(undefined)
+        setSize(undefined)
+    }, [product])
+
+    const check = () => {
+        if (color === undefined) {
+            alert('Choose a color')
+            return false
+        }
+
+        if (size === undefined) {
+            alert('Choose a size')
+            return false
+        }
+
+        return true
+    }
+
+    const addToCart = () => {
+        if (check()) {
+            let newItem = {
+                slug: product.slug,
+                color: color,
+                size: size,
+                price: product.price,
+                quantity: quantity
+            }
+            if (dispatch(addItem(newItem))) {
+                alert('Success')
+            } else {
+                alert('Fail')
+            }
+        }
+    }
+
+    const goToCart = () => {
+        if (check()) {
+            let newItem = {
+                slug: product.slug,
+                color: color,
+                size: size,
+                price: product.price,
+                quantity: quantity
+            }
+            if (dispatch(addItem(newItem))) {
+                dispatch(remove())
+                props.history.push('/cart')
+            } else {
+                alert('Fail')
+            }
+        }
+    }
+
+    return (
+        <div className="product">
+            <div className="product_images">
+                <div className="product_images_list">
+                    <div className="product_images_list_item" onClick={() => setPreviewImg(product.image01)}>
+                        <img src={product.image01} alt="" />
+                    </div>
+
+                
+                </div>
+                <div className="product_images_main">
+                    <img src={previewImg} alt="" />
+                </div>
+                <div className={`product-description ${descriptionExpand ? 'expand' : ''}`}>
+                    <div className="product-description_title">
+                        Product Details
+                    </div>
+                    <div className="product-description_content" dangerouslySetInnerHTML={{_html: product.description}}></div>
+                    <div className="product-description_toggle">
+                        <Button size="sm" onClick={() => setDescriptionExpand(!descriptionExpand)}>
+                            {
+                                descriptionExpand ? 'Collapse' : 'See more'
+                            }
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            <div className="product_info">
+                <h1 className="product_info_title">{product.title}</h1>
+                <div className="product_info_item">
+                    <span className="product_info_item_price">
+                        {numberWithCommas(product.price)}
+                    </span>
+                </div>
+                <div className="product_info_item">
+                    <div className="product_info_item_title">
+                        Color
+                    </div>
+                    <div className="product_info_item_list">
+                        {
+                            product.colors.map((item, index) => (
+                                <div key={index} className={`product_info_item_list_item ${color === item ? 'active' : ''}`} onClick={() => setColor(item)}>
+                                    <div className={`circle bg-${item}`}></div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="product_info_item">
+                    <div className="product_info_item_title">
+                        Size
+                    </div>
+                    <div className="product_info_item_list">
+                        {
+                            product.size.map((item, index) => (
+                                <div key={index} className={`product_info_item_list_item ${size === item ? 'active' : ''}`} onClick={() => setSize(item)}>
+                                    <span className="product_info_item_list_item_size">
+                                        {item}
+                                    </span>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="product_info_item">
+                    <div className="product_inf_item_title">
+                        Amount
+                    </div>
+                    <div className="product_info_item_quantity">
+                        <div className="product_info_item_quantity_btn" onClick={() => updateQuantity('minus')}>
+                            <i className="bx bx-minus"></i>
+                        </div>
+                        <div className="product_info_item_quantity_input">
+                            {quantity}
+                        </div>
+                        <div className="product_info_item_quantity_btn" onClick={() => updateQuantity('plus')}>
+                            <i className="bx bx-plus"></i>
+                        </div>
+                    </div>
+                </div>
+                <div className="product_info_item">
+                    <Button onClick={() => addToCart()}>Add to Cart</Button>
+                    <Button onClick={() => goToCart()}>Buy Now</Button>
+                </div>
+            </div>
+            <div className={`product-description mobile ${descriptionExpand ? 'expand' : ''}`}>
+                <div className="product-description_title">
+                    Product Details
+                </div>
+                <div className="product-description_content" dangerouslySetInnerHTML={{_html: product.description}}></div>
+                <div className="product-description_toggle">
+                    <Button size="sm" onClick={() => setDescriptionExpand(!descriptionExpand)}>
+                        {
+                            descriptionExpand ? 'Collapse' : 'See More'
+                        }
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ProductView.propTypes = {
+    product: PropTypes.object
+}
+
+export default withRouter(ProductView)
