eeb9f5c41d62be148704d7c99e642c73ccf293dc
chrisimata21 <chrisimata21@gmail.com>
2022-12-06 21:58:02 -0800

    sign up page is up and running properly

--- src/components/Login/signup.component.js
+++ src/components/Login/signup.component.js
@@ -1,43 +1,72 @@
-import React, { Component } from 'react'
-export default class SignUp extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-      </form>
-    )
-  }
+import React, { Component, useState } from "react";
+export default function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form>
+      <h3>Sign Up</h3>
+      <div className="mb-3">
+        <label>First name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Last name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="d-grid">
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={(e) => {
+            e.preventDefault();
+            console.log("test");
+            fetch("http://localhost:5000/register", {
+              method: "POST",
+              body: JSON.stringify({ firstName, lastName, email, password }),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+          }}
+        >
+          Sign Up
+        </button>
+      </div>
+    </form>
+  );
 }
