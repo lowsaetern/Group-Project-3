57de6358c44a9345947a01bb9cc1d83c8b0b76cd
chrisimata21 <chrisimata21@gmail.com>
2022-12-06 21:51:59 -0800

    connected to mongodb database

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Db = process.env.ATLAS_URI;
 
 (async () => {
-  await mongoose.connect(Db, { useMongoClient: true }, async () => {
-    console.log("Connected to Database");
+  await mongoose.connect(Db, {}, async () => {
+    console.log("Connected to Database", Db);
   });
 })();
