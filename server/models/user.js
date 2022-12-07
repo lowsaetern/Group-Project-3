6728765f1930bf3c0704658ae212d872253293ee
chrisimata21 <chrisimata21@gmail.com>
2022-12-06 21:52:49 -0800

    updated user.js model with correct schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,23 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
