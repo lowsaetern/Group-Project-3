34cd70d33169c8012d8c3896eb2240a8a6575164
nHur2004 <nathan.hur04@gmail.com>
2022-12-07 23:02:39 -0700

    Added a Contact Form to the Contact Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import '../css/header.css'
 
 import logo from '../assets/images/logo.png'
 
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ContactForm from "../components/ContactForm"
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const Contact = () => {
+    return (
+        <div className="Contact">
+            <ContactForm />
+        </div>
+    )
+}
+
+export default Contact
