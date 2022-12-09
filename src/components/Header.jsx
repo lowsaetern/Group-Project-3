3a71a35159c4052fafe19fe9f4ca0275fc11f760
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-12-08 18:34:15 -0800

    Search bar in header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,7 +78,10 @@ const Header = () => {
           </div>
           <div className="header_menu_right">
             <div className="header_menu_item header_menu_right_item">
-              <i className="bx bx-search"></i>
+             <i className="bx bx-search"></i>
+                <input type="text"
+                className="search"
+                placeholder="Search here..."></input>
             </div>
             <div className="header_menu_item header_menu_right_item">
               <Link to="/cart">
